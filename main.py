bb111441c88e09cfb1f5904c95368bf35facbd9c
Yejun Shin <91703836+YejunShin@users.noreply.github.com>
2021-11-18 19:47:13 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,34 +6,32 @@ wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image)
 wn.bgpic("background.gif") 
-appleList = [] 
+appleList = []
 appleLetters = []
 letters = ['A', 'S', 'D', 'F']
 for i in range(5): 
     appleList.append(trtl.Turtle())
     appleLetters.append(rand.choice(letters))
-apple=trtl.Turtle 
-appleLetter = 'A'
 #-----functions-----#
-def draw_apple(index): 
-    appleList[index].pu() 
-    appleList[index].shape(apple_image) 
-    wn.tracer(False) 
-    appleList[index].setx(rand.randint(-175,175))
-    appleList[index].sety(rand.randint(-25,100)) 
-    appleList[index].sety(appleList[index].ycor()-35) 
-    appleList[index].color("white") 
-    appleList[index].write(appleLetters[index], align="center",font=("Arial", 40, "bold")) 
-    appleList[index].sety(appleList[index].ycor()+35) 
-    appleList[index].showturtle() 
-    wn.tracer(True) 
-    wn.update() 
+def draw_apple(index):
+  appleList[(index)].pu() 
+  appleList[index].shape(apple_image) 
+  wn.tracer(False) 
+  appleList[index].setx(rand.randint(-175,175))
+  appleList[index].sety(rand.randint(-25,100)) 
+  appleList[index].sety(appleList[index].ycor()-35) 
+  appleList[index].color("white") 
+  appleList[index].write(appleLetters[index],align="center",font=("Arial", 40, "bold")) 
+  appleList[index].sety(appleList[index].ycor()+35) 
+  appleList[index].showturtle() 
+  wn.tracer(True) 
+  wn.update() 
 def drop_apple(index): 
     appleList[index].pu() 
     appleList[index].clear() 
     appleList[index].sety(-150) 
     appleList[index].hideturtle() 
-    draw_apple(index) 
+    draw_apple(index)
 def typedA(): 
     for i in range(5): 
         if appleLetters[i] == 'A':
@@ -50,10 +48,11 @@ def typedF():
     for i in range(5): 
         if appleLetters[i] == 'F': 
             drop_apple(i) 
-draw_apple(apple) 
+for i in range(5):
+  draw_apple(i)
 wn.onkeypress(typedA, 'a') 
 wn.onkeypress(typedS, 's') 
 wn.onkeypress(typedD, 'd') 
 wn.onkeypress(typedF, 'f') 
 wn.listen() 
-wn.mainloop() 
+wn.mainloop()
