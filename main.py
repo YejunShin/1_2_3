87ae6ceed77b9801a7c6ed545f449785d4f4dcd2
Yejun Shin <91703836+YejunShin@users.noreply.github.com>
2021-11-18 18:53:56 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,59 @@
-#   a123_apple_1.py
-import turtle as trtl
-
-#-----setup-----
-apple_image = "apple.gif" # Store the file name of your shape
-pear_image="pear.gif"
-wn = trtl.Screen()
+# a123_apple_1.py 
+import turtle as trtl 
+import random as rand 
+apple_image = "apple.gif"
+wn = trtl.Screen() 
 wn.setup(width=1.0, height=1.0)
-wn.addshape(apple_image) # Make the screen aware of the new file
-wn.addshape(pear_image)
-wn.bgpic("background.gif")
-apple = trtl.Turtle()
-pear=trtl.Turtle()
-drawer=trtl.Turtle()
-pear.hideturtle()
-#-----functions-----
-# given a turtle, set that turtle to be shaped by the image file
-def draw_apple(active_apple):
-  active_apple.shape(apple_image)
-  wn.update()
-def draw_pear(active_pear):
-  active_pear.shape(pear_image)
-  wn.update()
-def drop_apple():
-	apple.penup()
-	apple.goto(0,-150)
-def draw_an_A():
-	drawer.penup()
-	drawer.goto(-15,-45)
-	drawer.color("white")
-	drawer.write("A", font=("Arial", 40, "bold"))
-def events():
-	drop_apple()
-	drawer.clear()
-	apple.clear()
-#-----function calls-----
-draw_apple(apple)
-draw_an_A()
-wn.onkeypress(events, "a")
-wn.listen()
-wn.mainloop()
+wn.addshape(apple_image)
+wn.bgpic("background.gif") 
+appleList = [] 
+appleLetters = []
+letters = ['A', 'S', 'D', 'F']
+for i in range(5): 
+    appleList.append(trtl.Turtle())
+    appleLetters.append(rand.choice(letters))
+apple=trtl.Turtle 
+appleLetter = 'A'
+#-----functions-----#
+def draw_apple(index): 
+    appleList[index].pu() 
+    appleList[index].shape(apple_image) 
+    wn.tracer(False) 
+    appleList[index].setx(rand.randint(-175,175))
+    appleList[index].sety(rand.randint(-25,100)) 
+    appleList[index].sety(appleList[index].ycor()-35) 
+    appleList[index].color("white") 
+    appleList[index].write(appleLetters[index], align="center",font=("Arial", 40, "bold")) 
+    appleList[index].sety(appleList[index].ycor()+35) 
+    appleList[index].showturtle() 
+    wn.tracer(True) 
+    wn.update() 
+def drop_apple(index): 
+    appleList[index].pu() 
+    appleList[index].clear() 
+    appleList[index].sety(-150) 
+    appleList[index].hideturtle() 
+    draw_apple(index) 
+def typedA(): 
+    for i in range(5): 
+        if appleLetters[i] == 'A':
+            drop_apple(i) 
+def typedS(): 
+    for i in range(5): 
+        if appleLetters[i] == 'S': 
+            drop_apple(i) 
+def typedD(): 
+    for i in range(5): 
+        if appleLetters[i] == 'D': 
+            drop_apple(i) 
+def typedF(): 
+    for i in range(5): 
+        if appleLetters[i] == 'F': 
+            drop_apple(i) 
+draw_apple(apple) 
+wn.onkeypress(typedA, 'a') 
+wn.onkeypress(typedS, 's') 
+wn.onkeypress(typedD, 'd') 
+wn.onkeypress(typedF, 'f') 
+wn.listen() 
+wn.mainloop() 
